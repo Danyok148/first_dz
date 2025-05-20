15a563a1e34274a52c77ce83aeaf17063a9557cd
Danyok148 <berezhnyidanulo@gmail.com>
2025-05-20 17:44:18 +0300

    Дз 5

--- first_dz/first_dz.cpp
+++ first_dz/first_dz.cpp
@@ -1,88 +1,132 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main() {
-    const int SIZE = 20;
-    int arr[SIZE];
+    const int ROWS = 5;
+    const int COLS = 5;
+    int arr[ROWS][COLS];
 
-    
-    arr[0] = 0;
-    arr[1] = 1;
-    for (int i = 2; i < SIZE; i++) {
-        arr[i] = arr[i - 1] + arr[i - 2];
+    srand(time(0)); 
+
+    cout << "Масив:\n";
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            arr[i][j] = rand() % 101 - 50;
+            cout << arr[i][j] << "\t";
+        }
+        cout << endl;
     }
 
-    cout << "Масив Фiбонначi:\n";
-    for (int i = 0; i < SIZE; i++) {
-        cout << arr[i] << " ";
+    int col;
+    cout << "\nВведiть номер стовпця (0 - " << COLS - 1 << "): ";
+    cin >> col;
+    while (col < 0 || col >= COLS) {
+        cout << "Невiрно. Введiть ще раз: ";
+        cin >> col;
     }
-    cout << endl;
-
-   
-    cout << "\nПарнi значення:\n";
-    for (int i = 0; i < SIZE; i++) {
-        if (arr[i] % 2 == 0) {
-            cout << arr[i] << " ";
+    int maxCol = arr[0][col];
+    for (int i = 1; i < ROWS; i++) {
+        if (arr[i][col] > maxCol) {
+            maxCol = arr[i][col];
         }
     }
-    cout << endl;
+    cout << "Максимум у стовпцi " << col << " = " << maxCol << endl;
 
-    
-    cout << "\nВiд'ємнi значення:\n";
-    for (int i = 0; i < SIZE; i++) {
-        if (arr[i] < 0) {
-            cout << arr[i] << " ";
+    int row;
+    cout << "\nВведiть номер рядка (0 - " << ROWS - 1 << "): ";
+    cin >> row;
+    while (row < 0 || row >= ROWS) {
+        cout << "Невiрно. Введiть ще раз: ";
+        cin >> row;
+    }
+    int maxRow = arr[row][0];
+    for (int j = 1; j < COLS; j++) {
+        if (arr[row][j] > maxRow) {
+            maxRow = arr[row][j];
         }
     }
-    cout << "(немає вiд'ємних, бо ряд Фiбонначi завжди >= 0)\n";
+    cout << "Максимум у рядку " << row << " = " << maxRow << endl;
 
-    
-    int sum = 0;
-    for (int i = 0; i < SIZE; i++) {
-        sum += arr[i];
+    cout << "\nВведiть номер стовпця для пiдрахункiв: ";
+    cin >> col;
+    while (col < 0 || col >= COLS) {
+        cout << "Невiрно. Введiть ще раз: ";
+        cin >> col;
     }
-    cout << "\nСума елементiв масиву: " << sum << endl;
-
-    
-    double average = sum / (double)SIZE;
-    cout << "Середнє значення: " << average << endl;
-
-    
-    int search;
-    cout << "\nВведiть число для пошуку: ";
-    cin >> search;
-    bool found = false;
-    for (int i = 0; i < SIZE; i++) {
-        if (arr[i] == search) {
-            cout << "Число знайдено. Iндекс: " << i << endl;
-            found = true;
-            break;
+    int negSum = 0;
+    int posSum = 0, posCount = 0;
+    for (int i = 0; i < ROWS; i++) {
+        if (arr[i][col] < 0) {
+            negSum += arr[i][col];
+        }
+        else {
+            posSum += arr[i][col];
+            posCount++;
         }
     }
-    if (!found) {
-        cout << "Числа не є частиною массива.\n";
+    cout << "Сума негативних у стовпцi: " << negSum << endl;
+    if (posCount > 0) {
+        double avgPos = (double)posSum / posCount;
+        cout << "Середнє позитивних у стовпцi: " << avgPos << endl;
+    }
+    else {
+        cout << "Позитивних значень немає.\n";
     }
 
-    
-    int max = arr[0];
-    int min = arr[0];
-    for (int i = 1; i < SIZE; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
+    int col1, col2;
+    cout << "\nВведiть два номери стовпцiв для обмiну: ";
+    cin >> col1 >> col2;
+    if (col1 >= 0 && col1 < COLS && col2 >= 0 && col2 < COLS) {
+        for (int i = 0; i < ROWS; i++) {
+            int temp = arr[i][col1];
+            arr[i][col1] = arr[i][col2];
+            arr[i][col2] = temp;
         }
-        if (arr[i] < min) {
-            min = arr[i];
-        }
+        cout << "Стовпцi обмiняно.\n";
     }
-    cout << "\nМаксимальне значення: " << max << endl;
-    cout << "Мiнiмальне значення: " << min << endl;
+    else {
+        cout << "Неправильнi номери стовпцiв.\n";
+    }
 
-   
-    cout << "\nМасив у зворотньому порядку:\n";
-    for (int i = SIZE - 1; i >= 0; i--) {
-        cout << arr[i] << " ";
+    int row1, row2;
+    cout << "\nВведiть два номери рядкiв для обмiну: ";
+    cin >> row1 >> row2;
+    if (row1 >= 0 && row1 < ROWS && row2 >= 0 && row2 < ROWS) {
+        for (int j = 0; j < COLS; j++) {
+            int temp = arr[row1][j];
+            arr[row1][j] = arr[row2][j];
+            arr[row2][j] = temp;
+        }
+        cout << "Рядки обмiняно.\n";
     }
-    cout << endl;
+    else {
+        cout << "Неправильнi номери рядкiв.\n";
+    }
+
+    int rowRev;
+    cout << "\nВведiть номер рядка для реверсу: ";
+    cin >> rowRev;
+    if (rowRev >= 0 && rowRev < ROWS) {
+        for (int j = 0; j < COLS / 2; j++) {
+            int temp = arr[rowRev][j];
+            arr[rowRev][j] = arr[rowRev][COLS - 1 - j];
+            arr[rowRev][COLS - 1 - j] = temp;
+        }
+        cout << "Рядок реверсовано.\n";
+    }
+    else {
+        cout << "Невiрний номер рядка.\n";
+    }
+
+    cout << "\nОновлений масив:\n";
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            cout << arr[i][j] << "\t";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
